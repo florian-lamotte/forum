ed9008308458aab258d4e49d10d9bc8ac050e810
florian-lamotte <fl.lamotte@gmail.com>
2020-01-22 16:47:13 +0100

    first commit

---
+++ bulle/v2/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bulle</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<img src="../perso.jpg" id="perso" alt="perso" />
+
+	<div id="bulle">
+		<span>Bonjour !</span>
+		
+		<div id="fleche">
+			<span id="bordure_droite"></span>
+			<span id="masque1"></span>
+			<span id="bordure_gauche"></span>
+			<span id="masque2"></span>
+		</div>
+	</div>
+</body>
+</html>
---
+++ bulle/v3/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bulle</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<img src="../perso.jpg" id="perso" alt="perso" />
+
+	<div id="bulle">
+		<span>Bonjour !</span>
+		<span id="triangle"></span>
+		<span id="bordure_triangle"></span>
+	</div>
+</body>
+</html>
